898259fc5dcf71493f5fb477ad8957c2b68b2167
korov <korova2321@gmail.com>
2024-02-17 09:11:40 +0100

    Fix warning

--- PrimitiveJsonConverterGenerator/Extensions/IndentedTextWriterExtensions.cs
+++ PrimitiveJsonConverterGenerator/Extensions/IndentedTextWriterExtensions.cs
@@ -35,11 +35,15 @@ internal static class IndentedTextWriterExtensions
     {
         writer.WriteLine("// Disable CS1591 - Missing XML comment on public member");
         writer.WriteLine("#pragma warning disable 1591");
+        writer.WriteLine("// Disable CS8604 - Possible null reference argument for parameter.");
+        writer.WriteLine("#pragma warning disable 8604");
     }
 
     public static void WritePragmaWarningRestoreLine(this IndentedTextWriter writer)
     {
         writer.WriteLine("// Disable CS1591 - Missing XML comment on public member");
         writer.WriteLine("#pragma warning restore 1591");
+        writer.WriteLine("// Disable CS8604 - Possible null reference argument for parameter.");
+        writer.WriteLine("#pragma warning restore 8604");
     }
 }
