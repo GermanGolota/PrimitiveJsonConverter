ed3ce2dffa10f80379293f4f4db177b6f4865762
korov <korova2321@gmail.com>
2024-02-20 15:35:44 +0100

    Fix tests

--- tests/PrimitiveJsonConverter.Generator.Tests/Snapshots/PrimitiveJsonConverterTests.GeneratesEnumExtensionsCorrectly#PrimitiveJsonConverterLoader.g.verified.cs
+++
@@ -1,8 +0,0 @@
-﻿//HintName: PrimitiveJsonConverterLoader.g.cs
-namespace PrimitiveJsonConverter.Generator
-{
-    internal static partial class PrimitiveJsonConverterLoader
-    {
-        public static partial void Load();
-    }
-}
