0248c6b32d7fc6cbd2dd67254a2c53d74d704d5a
korov <korova2321@gmail.com>
2024-03-08 22:25:46 +0100

    Improve sample

---
+++ sample/PrimitiveJsonConverter.Generator.Sample/EnvironmentName.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json.Serialization;
+
+namespace PrimitiveJsonConverter.Generator.Sample;
+
+[JsonPrimitiveConverter(typeof(EnvironmentName))]
+internal sealed partial class EnvironmentNameConverter { }
+
+[JsonConverter(typeof(EnvironmentNameConverter))]
+public sealed record EnvironmentName
+{
+    public EnvironmentName(string value)
+    {
+        if (string.IsNullOrEmpty(value))
+        {
+            throw new ArgumentOutOfRangeException(nameof(value));
+        }
+
+        Value = value;
+    }
+
+    public string Value { get; }
+
+    public static implicit operator EnvironmentName(string value) => new(value);
+    public static implicit operator string(EnvironmentName value) => value.Value;
+}
