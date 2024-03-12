851ae6e985c5b716edbf5e106fbe49ffa4883a15
korov <korova2321@gmail.com>
2024-03-12 08:39:28 +0100

    Update tests

--- tests/PrimitiveJsonConverter.Generator.Tests/PrimitiveJsonConverterTests.cs
+++ tests/PrimitiveJsonConverter.Generator.Tests/PrimitiveJsonConverterTests.cs
@@ -3,7 +3,7 @@ namespace PrimitiveJsonConverter.Generator.Tests;
 public class PrimitiveJsonConverterTests
 {
     [Fact]
-    public Task CreateClass_Ok()
+    public Task CreateClass_WhenValue_Ok()
     {
         // The source code to test
         var source = """
@@ -41,7 +41,49 @@ public sealed partial record DiceRoll
     }
 
     [Fact]
-    public Task CreateConverter_Ok()
+    public Task CreateClass_WhenNullableValue_Ok()
+    {
+        // The source code to test
+        var source = """
+using PrimitiveJsonConverter;
+
+[JsonPrimitive]
+public sealed partial record DiceRoll
+{
+    public DiceRoll(int value)
+    {
+        if (value <= 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(value));
+        }
+
+        Value = value;
+    }
+
+    public int Value { get; }
+
+    public static implicit operator DiceRoll?(int? value)
+    {
+        if(value.HasValue)
+        {
+            return new(value.Value);
+        }
+        return null;
+    }
+
+    public static implicit operator int?(DiceRoll? value)
+    {
+        return value?.Value;
+    }
+}
+""";
+
+        // Pass the source code to our helper and snapshot test the output
+        return TestHelper.Verify(source);
+    }
+
+    [Fact]
+    public Task CreateConverter_WhenValue_Ok()
     {
         // The source code to test
         var source = """
@@ -73,4 +115,36 @@ public sealed record DiceRoll
         // Pass the source code to our helper and snapshot test the output
         return TestHelper.Verify(source);
     }
+    
+    [Fact]
+    public Task CreateClass_WhenReference_Ok()
+    {
+        // The source code to test
+        var source = """
+using PrimitiveJsonConverter;
+
+[JsonPrimitive]
+public sealed partial record FileName
+{
+    public FileName(string value)
+    {
+        if (string.IsNullOrEmpty(value))
+        {
+            throw new ArgumentOutOfRangeException(nameof(value));
+        }
+
+        Value = value;
+    }
+
+    public string Value { get; }
+
+    public static implicit operator FileName?(string? value) => value is null ? null : new(value);
+    public static implicit operator string?(FileName? value) => value?.Value;
+}
+""";
+
+        // Pass the source code to our helper and snapshot test the output
+        return TestHelper.Verify(source);
+    }
+
 }
